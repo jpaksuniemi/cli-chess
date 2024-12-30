cce47c93dd3d6cd08d6060aa7d6090f6e3132b16
Janne Paksuniemi <jannepasp@gmail.com>
2024-12-30 23:39:45 +0200

    Class for constant values implemented

---
+++ src/main/java/pksn/janne/util/ConstantValues.java
@@ -0,0 +1,7 @@
+package pksn.janne.util;
+
+public class ConstantValues {
+    public static final int DEFAULT_BOARD_SIZE = 8;
+    public static final int UPPERCASE_A_ASCII_VALUE = 65;
+    public static final int UPPERCASE_H_ASCII_VALUE = 72;
+}
