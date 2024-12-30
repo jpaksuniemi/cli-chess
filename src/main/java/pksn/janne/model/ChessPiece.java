9d62f20fa0e69e9fb73bb427d616352a9359b989
Janne Paksuniemi <jannepasp@gmail.com>
2024-12-30 21:45:17 +0200

    Changed ChessPiece into an abstract class

--- src/main/java/pksn/janne/model/ChessPiece.java
+++ src/main/java/pksn/janne/model/ChessPiece.java
@@ -1,5 +1,32 @@
 package pksn.janne.model;
 
-public interface ChessPiece {
-    public void move();
+import pksn.janne.model.ChessBoard.Column;
+
+public abstract class ChessPiece {
+
+    private int currRow;
+    private Column currColumn;
+
+    public ChessPiece(int currRow, Column currColumn) {
+        this.currRow = currRow;
+        this.currColumn = currColumn;
+    }
+
+    public int getCurrRow() {
+        return currRow;
+    }
+
+    public void setCurrRow(int currRow) {
+        this.currRow = currRow;
+    }
+
+    public Column getCurrColumn() {
+        return currColumn;
+    }
+
+    public void setCurrColumn(Column currColumn) {
+        this.currColumn = currColumn;
+    }
+    
+    public abstract boolean isValidMove(int row, Column column);
 }
