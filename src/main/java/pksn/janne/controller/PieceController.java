fa6886110e076d8e37691c56beee8a24d312db6f
Janne Paksuniemi <jannepasp@gmail.com>
2025-01-14 22:04:54 +0200

    Pieces can now take other pieces out

--- src/main/java/pksn/janne/controller/PieceController.java
+++ src/main/java/pksn/janne/controller/PieceController.java
@@ -9,12 +9,17 @@ public class PieceController {
 
     public static boolean moveChessPiece(ChessPiece piece, int row, Character column) {
         if (!piece.isValidMove(row, column)) { return false; }
-        if (board.get(row, column) != null) { return false; }
+        boolean test = canTakePiece(piece, board.get(row, column));
+        if (board.get(row, column) != null && !canTakePiece(piece, board.get(row, column))) { return false; }
         board.add(null, piece.getCurrRow(), piece.getCurrColumn());
         board.add(piece, row, column);
         piece.setCurrRow(row);
         piece.setCurrColumn(column);
         return true;
+    }
+
+    private static boolean canTakePiece(ChessPiece taker, ChessPiece toBeTaken) {
+        return toBeTaken != null && taker.getColor() != toBeTaken.getColor();
     }
 
     public static void setBoard(ChessBoard chessBoard) {
