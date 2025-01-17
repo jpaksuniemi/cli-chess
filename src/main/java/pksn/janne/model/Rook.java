6714edc1d9340f6a2fed183829361e13001646c2
Janne Paksuniemi <jannepasp@gmail.com>
2025-01-17 22:14:56 +0200

    Rook updated to use MovementType classes

--- src/main/java/pksn/janne/model/Rook.java
+++ src/main/java/pksn/janne/model/Rook.java
@@ -1,22 +1,14 @@
 package pksn.janne.model;
 
-import pksn.janne.util.BoardHelper;
-
 public class Rook extends ChessPiece {
 
     public Rook(int row, Character column, Color color) {
-        super(row, column, color);
+        super(row, column, color, new OrthogonalMovement());
     }
 
     @Override
     public boolean isValidMove(int row, Character column) {
-        if (!BoardHelper.isValidColumn(column) || !BoardHelper.isValidRow(row)) {
-            return false; 
-        }
-        if ( (getCurrColumn() == column && getCurrRow() != row) || (getCurrColumn() != column && getCurrRow() == row) ) {
-            return true;
-        }
-        return false;
+        return movementType.isValidMove(currRow, currColumn, row, column);
     }
 
     @Override
