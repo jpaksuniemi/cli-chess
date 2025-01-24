4973885a49aee73b3ce6be3204a0dd34e7d19e1c
Janne Paksuniemi <jannepasp@gmail.com>
2025-01-24 12:51:24 +0200

    Bishop implemented

---
+++ src/main/java/pksn/janne/model/Bishop.java
@@ -0,0 +1,13 @@
+package pksn.janne.model;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(int row, Character column, Color color) {
+        super(row, column, color, new DiagonalMovement());
+    }
+
+    @Override
+    public boolean isValidMove(int row, Character column) {
+        return movementType.isValidMove(currRow, currColumn, row, column);
+    }
+}
